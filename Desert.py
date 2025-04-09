9025b93e3cb0ab6f2b88251ff71647a27cf7145e
CoffeeBeanNyla <CoffeeBeanNyla@users.noreply.github.com>
2025-04-09 16:34:47 -0400

    Added more clouds to the background

--- Desert.py
+++ Desert.py
@@ -42,23 +42,54 @@ while running:
     # Set screen color
     screen.fill("cyan")
     
+    #The Sun
     pygame.draw.circle(screen, (254,189,89,255), (320,40), 100)
-    
-    pygame.draw.circle(screen, "white", (20,63), 33)
-    pygame.draw.circle(screen, "white", (50,44), 30)
+      #left side of clouds
+    pygame.draw.circle(screen, "white", (30,63), 33)
+    pygame.draw.circle(screen, "white", (54,44), 30)
     pygame.draw.circle(screen, "white", (80,63), 32)
     
     pygame.draw.circle(screen, "white", (200,80), 23)
     pygame.draw.circle(screen, "white", (160,84), 23)
     pygame.draw.circle(screen, "white", (178,70), 20)
-        
-    pygame.draw.circle(screen, "white", (500,63), 36)
-    pygame.draw.circle(screen, "white", (530,34), 32)
-    pygame.draw.circle(screen, "white", (560,63), 38)
     
+    pygame.draw.circle(screen, "white", (120,130), 13)
+    pygame.draw.circle(screen, "white", (130,123), 10)
+    pygame.draw.circle(screen, "white", (140,132), 12)
+    
+    pygame.draw.circle(screen, "white", (156,30), 13)
+    pygame.draw.circle(screen, "white", (166,20), 10)
+    pygame.draw.circle(screen, "white", (176,30), 12)
+    
+    pygame.draw.circle(screen, "white", (36,116), 12)
+    pygame.draw.circle(screen, "white", (46,109), 10)
+    pygame.draw.circle(screen, "white", (56,117), 11)
+    
+    #right side of clouds
     pygame.draw.circle(screen, "white", (400,112), 23)
     pygame.draw.circle(screen, "white", (360,115), 23)
     pygame.draw.circle(screen, "white", (379,98), 20)
+    
+    pygame.draw.circle(screen, "white", (500,63), 36)
+    pygame.draw.circle(screen, "white", (530,34), 32)
+    pygame.draw.circle(screen, "white", (560,63), 38)
+      
+    pygame.draw.circle(screen, "white", (496,132), 13)
+    pygame.draw.circle(screen, "white", (485,121), 10)
+    pygame.draw.circle(screen, "white", (474,130), 14)
+    
+    pygame.draw.circle(screen, "white", (600,132), 13)
+    pygame.draw.circle(screen, "white", (589,121), 10)
+    pygame.draw.circle(screen, "white", (579,130), 14)
+    
+    pygame.draw.circle(screen, "white", (430,30), 13)
+    pygame.draw.circle(screen, "white", (441,20), 10)
+    pygame.draw.circle(screen, "white", (452,30), 12)
+    
+    pygame.draw.circle(screen, "white", (434,80), 11)
+    pygame.draw.circle(screen, "white", (442,70), 9)
+    pygame.draw.circle(screen, "white", (452,80), 10)
+    
 
     # Draw a rectangle
     pygame.draw.rect(screen, (234,182,118), (0, 150, 640,480))
@@ -76,13 +107,14 @@ while running:
     
     #enemy_box = pygame.draw.rect(screen, (227,241,241,255), (enemy, (50,50)))
     
-    if player_box.colliderect(enemy_box):
-        running = False
+    #if player_box.colliderect(enemy_box):
+        #running = False
                 
-    if player_box.colliderect(enemy_box):
-        print(player_box)
-        print(enemy_box)
-        running = False
+    #if player_box.colliderect(enemy_box):
+        #print(player_box)
+        #print(enemy_box)
+        #running = False
+    
     # update player position based on keys pressed
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:

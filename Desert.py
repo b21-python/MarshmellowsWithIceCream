4a7cb786b6ed0355ed89751709e9bc511164d9d2
CoffeeBeanNyla <CoffeeBeanNyla@users.noreply.github.com>
2025-05-21 13:44:46 -0400

    Added clouds as it's own file and made a win and lose

--- Desert.py
+++ Desert.py
@@ -2,6 +2,7 @@ import pygame
 from math import pi
 from player_animation import cat
 from enemy_animation import kitten
+from clouds import clouds
 pygame.init() # initialize pygame
 
 # Create a game window that is 640 by 480 pixels
@@ -10,9 +11,15 @@ clock = pygame.time.Clock()
 running = True
 
 # create a variable to store player position so we can modify it based on keyboard input
-player_pos = pygame.Vector2(screen.get_width()/2, screen.get_height()/2)
+def player_start(screen):
+    return pygame.Vector2(screen.get_width()/2, screen.get_height()/2 - 40)
 
-enemy_pos = pygame.Vector2(0, 300)
+player_pos = player_start(screen)
+
+def enemy_start():
+    return pygame.Vector2(0, 300)
+
+enemy_pos = enemy_start()
 player_size = 34
 def cropAlpha(surface):
     final_size = surface.get_bounding_rect()
@@ -42,150 +49,118 @@ enemy_frame = 0
 #player = pygame.transform.scale(player, (120,100))
 font = pygame.font. SysFont('Ariel',42)
 game_over = False
-while running:
-    # poll for events and react to user closing the window to end the game
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+while True:
+    while running:
+        # poll for events and react to user closing the window to end the game
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        #if game_over == True:
+            #fontSurface = font.render("You win!", True, [0, 225, 0])
+        #else:
+            #fontSurface = font.render("You lose!", True, [225, 0, 0])
+        #screen.blit(fontSurface, [300, 200])
+        #pygame.display.flip()
+
+        # Set screen color
+        screen.fill("cyan")
+        
+        clouds(screen)
+        # Draw a rectangle
+        pygame.draw.rect(screen, (234,182,118), (0, 150, 640,480))
+        
+        #pygame.Rect( (60, 100), (80,80))
+        
+        player_box = screen.blit(player[player_frame], player_pos)
+        
+        enemy_box = screen.blit(enemy[0],enemy_pos)
+       
+        # Draw an ellipse
+        lake = pygame.Surface((480,180), pygame.SRCALPHA)
+        lake_width = 480
+        lake_height = 180
+        pygame.draw.ellipse(lake, (0,200,255, 170), (0, 0, lake_width, lake_height))
+        
+        lake_offset_pos_x = 110
+        lake_offset_pos_y = 230
+        lake_box = screen.blit(lake, (lake_offset_pos_x, lake_offset_pos_y))
+        
+        #enemy_box = pygame.draw.rect(screen, (227,241,241,255), (enemy, (50,50)))
+        
+        if player_box.colliderect(enemy_box):
             running = False
-    #if game_over == True:
-        #fontSurface = font.render("You win!", True, [0, 225, 0])
-    #else:
-        #fontSurface = font.render("You lose!", True, [225, 0, 0])
-    #screen.blit(fontSurface, [300, 200])
-    #pygame.display.flip()
+            game_over = True
+        if is_point_in_ellipse(player_pos.x, player_pos.y, lake_box.centerx, lake_box.centery, lake_width/2, lake_height/2):
+            running = False
+            game_over = True
+            print ("You're Dead")
+            print (player_pos)
+            
+        if is_point_in_ellipse(enemy_pos.x, enemy_pos.y, lake_box.centerx, lake_box.centery, lake_width/2, lake_height/2):
+            running = False
+            game_over = False
+            
+        # update player position based on keys pressed
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            player_pos.y -= 10
+            player_frame = (player_frame + 1) % 3 + 3
+            
+        if keys[pygame.K_s]:
+            player_pos.y += 10
+            player_frame = (player_frame + 1) % 3 + 6
 
-    # Set screen color
-    screen.fill("cyan")
-    
-    #The Sun
-    pygame.draw.circle(screen, (254,189,89,255), (320,40), 100)
-      #left side of clouds
-    pygame.draw.circle(screen, "white", (30,63), 33)
-    pygame.draw.circle(screen, "white", (54,44), 30)
-    pygame.draw.circle(screen, "white", (80,63), 32)
-    
-    pygame.draw.circle(screen, "white", (200,80), 23)
-    pygame.draw.circle(screen, "white", (160,84), 23)
-    pygame.draw.circle(screen, "white", (178,70), 20)
-    
-    pygame.draw.circle(screen, "white", (120,130), 13)
-    pygame.draw.circle(screen, "white", (130,123), 10)
-    pygame.draw.circle(screen, "white", (140,132), 12)
-    
-    pygame.draw.circle(screen, "white", (156,30), 13)
-    pygame.draw.circle(screen, "white", (166,20), 10)
-    pygame.draw.circle(screen, "white", (176,30), 12)
-    
-    pygame.draw.circle(screen, "white", (36,116), 12)
-    pygame.draw.circle(screen, "white", (46,109), 10)
-    pygame.draw.circle(screen, "white", (56,117), 11)
-    
-    #right side of clouds
-    pygame.draw.circle(screen, "white", (400,112), 23)
-    pygame.draw.circle(screen, "white", (360,115), 23)
-    pygame.draw.circle(screen, "white", (379,98), 20)
-    
-    pygame.draw.circle(screen, "white", (500,63), 36)
-    pygame.draw.circle(screen, "white", (530,34), 32)
-    pygame.draw.circle(screen, "white", (560,63), 38)
-      
-    pygame.draw.circle(screen, "white", (496,132), 13)
-    pygame.draw.circle(screen, "white", (485,121), 10)
-    pygame.draw.circle(screen, "white", (474,130), 14)
-    
-    pygame.draw.circle(screen, "white", (600,132), 13)
-    pygame.draw.circle(screen, "white", (589,121), 10)
-    pygame.draw.circle(screen, "white", (579,130), 14)
-    
-    pygame.draw.circle(screen, "white", (430,30), 13)
-    pygame.draw.circle(screen, "white", (441,20), 10)
-    pygame.draw.circle(screen, "white", (452,30), 12)
-    
-    pygame.draw.circle(screen, "white", (434,80), 11)
-    pygame.draw.circle(screen, "white", (442,70), 9)
-    pygame.draw.circle(screen, "white", (452,80), 10)
-    
+        if keys[pygame.K_a]:
+            player_pos.x -= 10
+            player_frame = (player_frame + 1) % 3 + 9
 
-    # Draw a rectangle
-    pygame.draw.rect(screen, (234,182,118), (0, 150, 640,480))
-    
-    #pygame.Rect( (60, 100), (80,80))
-    
-    player_box = screen.blit(player[player_frame], player_pos)
-    
-    enemy_box = screen.blit(enemy[0],enemy_pos)
+        if keys[pygame.K_d]:
+            player_pos.x += 10
+            player_frame = (player_frame + 1) % 3 
+            
+        if player_pos.x > enemy_pos.x:
+            enemy_pos.x += 1
+        if player_pos.x < enemy_pos.x:
+            enemy_pos.x -= 1
+        if player_pos.y > enemy_pos.y:
+            enemy_pos.y += 1
+        if player_pos.y < enemy_pos.y:
+            enemy_pos.y -= 1
+            
+        if player_pos.x <= -player_size:
+            player_pos.x = screen.get_width() - 1
+        if player_pos.x >= screen.get_width():
+            player_pos.x = -player_size + 1
+        if player_pos.y <= 150 - player_size:
+            player_pos.y = screen.get_height() - 1
+        if player_pos.y >= screen.get_height():
+            player_pos.y = 150 - player_size + 1
+        # Render what you've drawn to the screen
+        pygame.display.flip()
+
+        clock.tick(60) # limit to 60 FPS
+            
+        #font = pygame.font. SysFont('Ariel',42)
+        #game_over = False
+        #running = True
+        #while running:
+            #for event in pygame.event.get():
+                #if event.type == pygame.QUIT:
+                    #running = False
+    if game_over == True:
+        fontSurface = font.render("You lose!", True, [0, 225, 0])
+        screen.blit(fontSurface, [300, 200])
+        pygame.display.flip()
+    else:
+        fontSurface = font.render("You win!", True, [0, 225, 0])
+        screen.blit(fontSurface, [300, 200])
+        pygame.display.flip()
    
-    # Draw an ellipse
-    lake = pygame.Surface((480,180), pygame.SRCALPHA)
-    lake_width = 450
-    lake_height = 100
-    pygame.draw.ellipse(lake, (0,200,255, 170), (0, 20, lake_width, lake_height))
-    
-    lake_offset_pos_x = 110
-    lake_offset_pos_y = 230
-    lake_box = screen.blit(lake, (lake_offset_pos_x, lake_offset_pos_y))
-    
-    #enemy_box = pygame.draw.rect(screen, (227,241,241,255), (enemy, (50,50)))
-    
-    if player_box.colliderect(enemy_box):
-        running = False
-        game_over = True        
-   
-#     if enemy_box.colliderect(lake_box):
-        #running = False
-        
-    # update player position based on keys pressed
+    pygame.event.get()    
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 10
-        player_frame = (player_frame + 1) % 3 + 3
-        
-    if keys[pygame.K_s]:
-        player_pos.y += 10
-        player_frame = (player_frame + 1) % 3 + 6
-
-    if keys[pygame.K_a]:
-        player_pos.x -= 10
-        player_frame = (player_frame + 1) % 3 + 9
-
-    if keys[pygame.K_d]:
-        player_pos.x += 10
-        player_frame = (player_frame + 1) % 3 
-        
-    if player_pos.x > enemy_pos.x:
-        enemy_pos.x += 1
-    if player_pos.x < enemy_pos.x:
-        enemy_pos.x -= 1
-    if player_pos.y > enemy_pos.y:
-        enemy_pos.y += 1
-    if player_pos.y < enemy_pos.y:
-        enemy_pos.y -= 1
-        
-    if player_pos.x <= -player_size:
-        player_pos.x = screen.get_width() - 1
-    if player_pos.x >= screen.get_width():
-        player_pos.x = -player_size + 1
-    if player_pos.y <= -player_size:
-        player_pos.y = screen.get_height() - 1
-    if player_pos.y >= screen.get_height():
-        player_pos.y = -player_size + 1
-    # Render what you've drawn to the screen
-    pygame.display.flip()
-
-    clock.tick(60) # limit to 60 FPS
-    
-#font = pygame.font. SysFont('Ariel',42)
-#game_over = False
-#running = True
-#while running:
-    #for event in pygame.event.get():
-        #if event.type == pygame.QUIT:
-            #running = False
-if game_over == True:
-    fontSurface = font.render("You lose!", True, [0, 225, 0])
-    screen.blit(fontSurface, [300, 200])
-    pygame.display.flip()
-else:
-    pygame.quit()  
+    if keys[pygame.K_SPACE]:
+        running = True
+        player_pos = player_start(screen)
+        enemy_pos = enemy_start()
 # Exit game when we leave the game loop
 #pygame.quit()         
---
+++ clouds.py
@@ -0,0 +1,50 @@
+import pygame
+
+def clouds(screen):
+        #The Sun
+    pygame.draw.circle(screen, (254,189,89,255), (320,40), 100)
+      #left side of clouds
+    pygame.draw.circle(screen, "white", (30,63), 33)
+    pygame.draw.circle(screen, "white", (54,44), 30)
+    pygame.draw.circle(screen, "white", (80,63), 32)
+    
+    pygame.draw.circle(screen, "white", (200,80), 23)
+    pygame.draw.circle(screen, "white", (160,84), 23)
+    pygame.draw.circle(screen, "white", (178,70), 20)
+    
+    pygame.draw.circle(screen, "white", (120,130), 13)
+    pygame.draw.circle(screen, "white", (130,123), 10)
+    pygame.draw.circle(screen, "white", (140,132), 12)
+    
+    pygame.draw.circle(screen, "white", (156,30), 13)
+    pygame.draw.circle(screen, "white", (166,20), 10)
+    pygame.draw.circle(screen, "white", (176,30), 12)
+    
+    pygame.draw.circle(screen, "white", (36,116), 12)
+    pygame.draw.circle(screen, "white", (46,109), 10)
+    pygame.draw.circle(screen, "white", (56,117), 11)
+    
+    #right side of clouds
+    pygame.draw.circle(screen, "white", (400,112), 23)
+    pygame.draw.circle(screen, "white", (360,115), 23)
+    pygame.draw.circle(screen, "white", (379,98), 20)
+    
+    pygame.draw.circle(screen, "white", (500,63), 36)
+    pygame.draw.circle(screen, "white", (530,34), 32)
+    pygame.draw.circle(screen, "white", (560,63), 38)
+      
+    pygame.draw.circle(screen, "white", (496,132), 13)
+    pygame.draw.circle(screen, "white", (485,121), 10)
+    pygame.draw.circle(screen, "white", (474,130), 14)
+    
+    pygame.draw.circle(screen, "white", (600,132), 13)
+    pygame.draw.circle(screen, "white", (589,121), 10)
+    pygame.draw.circle(screen, "white", (579,130), 14)
+    
+    pygame.draw.circle(screen, "white", (430,30), 13)
+    pygame.draw.circle(screen, "white", (441,20), 10)
+    pygame.draw.circle(screen, "white", (452,30), 12)
+    
+    pygame.draw.circle(screen, "white", (434,80), 11)
+    pygame.draw.circle(screen, "white", (442,70), 9)
+    pygame.draw.circle(screen, "white", (452,80), 10)
